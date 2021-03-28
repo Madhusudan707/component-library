96db3fb7dbb45faaa5393bcd38a218cef914e5f6
Maddy@Xdev200 <xdev200200@gmail.com>
2021-03-28 11:54:53 +0530

    update:style & alignment

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -18,9 +18,11 @@ const Alert = () => {
             </div>
           
             
-            <Snippet code_snippet="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=seti&wt=none&l=auto&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=true&fl=1&fm=Hack&fs=12px&lh=131%25&si=false&es=2x&wm=false&code=%253Cspan%2520class%253D%27error%27%253E%253C%252Fspan%253E%250A%253Cspan%2520class%253D%27warning%27%253E%253C%252Fspan%253E%250A%253Cspan%2520class%253D%27info%27%253E%253C%252Fspan%253E%250A%253Cspan%2520class%253D%27success%27%253E%253C%252Fspan%253E"/>
+            <Snippet code_snippet="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=seti&wt=sharp&l=auto&ds=false&dsyoff=20px&dsblur=100px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cspan%2520class%253D%27error%27%253E%253C%252Fspan%253E%250A%250A%253Cspan%2520class%253D%27warning%27%253E%253C%252Fspan%253E%250A%250A%253Cspan%2520class%253D%27info%27%253E%253C%252Fspan%253E%250A%250A%253Cspan%2520class%253D%27success%27%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520"/>
         </div>
     )
 }
+
+
 
 export default Alert
