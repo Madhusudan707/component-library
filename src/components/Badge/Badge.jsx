4641720ccb659381ab0619804b9fc372ee2355a2
Maddy@Xdev200 <xdev200200@gmail.com>
2021-03-28 12:05:56 +0530

    update:style & alignment

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -16,9 +16,8 @@ const Badge = () => {
             </div>
           
             
-            <Snippet code_snippet="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=blackboard&wt=none&l=htmlmixed&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=56px&ph=56px&ln=true&fl=1&fm=Fira+Code&fs=14px&lh=154%25&si=false&es=2x&wm=false&code=%253Cspan%2520class%253D%2522badge%2522%253E1%253C%252Fspan%253E%250A%253Cspan%2520class%253D%2522badge%2522%253E%253Ci%2520class%253D%2522fa%2520fa-bell%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%253Cspan%2520class%253D%2522badge%2522%253E%253Ci%2520class%253D%2522fa%2520fa-inbox%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%253Cspan%2520class%253D%2522badge%2522%253EMaddy%253C%252Fspan%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520"/>
+            <Snippet code_snippet="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=seti&wt=sharp&l=auto&ds=false&dsyoff=20px&dsblur=100px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cspan%2520class%253D%2522badge%2522%253E1%253C%252Fspan%253E%250A%250A%253Cspan%2520class%253D%2522badge%2522%253E%253Ci%2520class%253D%2522fa%2520fa-bell%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%250A%253Cspan%2520class%253D%2522badge%2522%253E%253Ci%2520class%253D%2522fa%2520fa-inbox%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%250A%253Cspan%2520class%253D%2522badge%2522%253EMaddy%253C%252Fspan%253E%250A%2520%2520%2520%2520"/>
         </div>
     )
 }
-
 export default Badge
