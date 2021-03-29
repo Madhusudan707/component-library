f9ac17241e11c42d4623af83a70954e36816e52e
Maddy@Xdev200 <xdev200200@gmail.com>
2021-03-29 18:24:19 +0530

    updated: style and snippet

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -13,15 +13,15 @@ const Rating = () => {
             <div className={`${styles.content_block} ${styles.rating}`}>
                 <a href='#rating'>
                     
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
+                    <i className='fa fa-star-o'></i>
+                    <i className='fa fa-star-o'></i>
+                    <i className='fa fa-star-o'></i>
+                    <i className='fa fa-star-o'></i>
+                    <i className='fa fa-star-o'></i>
             </a>
                
             </div>
-            <Snippet code_snippet="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=blackboard&wt=none&l=htmlmixed&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=true&fl=1&fm=Fira+Code&fs=14px&lh=154%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%27rating%27%253E%250A%2520%2520%253Ci%2520className%253D%27fa%2520fa-star%27%253E%253C%252Fi%253E%250A%2520%2520%253Ci%2520className%253D%27fa%2520fa-star%27%253E%253C%252Fi%253E%250A%2520%2520%253Ci%2520className%253D%27fa%2520fa-star%27%253E%253C%252Fi%253E%250A%2520%2520%253Ci%2520className%253D%27fa%2520fa-star%27%253E%253C%252Fi%253E%250A%2520%2520%253Ci%2520className%253D%27fa%2520fa-star%27%253E%253C%252Fi%253E%250A%253C%252Fdiv%253E"/>
+            <Snippet code_snippet="https://carbon.now.sh/embed?bg=rgba%28255%2C255%2C255%2C1%29&t=seti&wt=sharp&l=auto&ds=false&dsyoff=20px&dsblur=100px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=148%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522rating%2522%253E%250A%2520%2520%2520%253Ca%2520href%253D%2522%2523rating%2522%253E%2520%2520%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fa%2520fa-star-o%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fa%2520fa-star-o%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fa%2520fa-star-o%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fa%2520fa-star-o%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fa%2520fa-star-o%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fa%253E%250A%2520%253C%252Fdiv%253E"/>
        
             
         </div>
