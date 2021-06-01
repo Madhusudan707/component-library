9a8ff6acd5576d79283e1a14fc799e8b47cf005e
Maddy@Xdev200 <xdev200200@gmail.com>
2021-06-01 10:08:49 +0530

    add: description

---
+++ src/Components/Description/Description.jsx
@@ -0,0 +1,7 @@
+
+import './description.css'
+export const Description = ({text}) => {
+    return (
+        <h4 className="description" >{text}</h4>
+    )
+}
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export {SideNav} from './SideNav/SideNav'
 export {Brand} from './Brand/Brand'
 export {DropDown} from './DropDown/DropDown'
+export {Description} from './Description/Description'
