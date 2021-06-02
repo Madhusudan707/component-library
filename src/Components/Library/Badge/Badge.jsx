c537cbc429e30085871ffb9c3de59722a921bae0
Maddy@Xdev200 <xdev200200@gmail.com>
2021-06-02 09:21:49 +0530

    update: Badge

--- src/Components/Library/Badge/Badge.jsx
+++ src/Components/Library/Badge/Badge.jsx
@@ -1,27 +1,40 @@
-
 import { Heading, Description, Snippet } from "../../";
 
-import './badge.css'
+import "./badge.css";
 export const Badge = () => {
-    return (
-        <>
-            <Heading text="Badge"/>
-            <Description text="Badges can notify you that there are new or unread messages or notifications."/>
-            <div className="content_block">
-                <span className="badge">1</span>
-                <span className="badge"><i className="fa fa-bell"></i></span>
-                <span className="badge"><i className="fa fa-inbox"></i></span>
-                <span className="badge">Maddy</span>
-            </div>
-          
-            
-            <Snippet code_snippet={`<span class="badge">1</span>
+  return (
+    <>
+      <Heading text="Badge" />
+      <Description text="Badges can notify you that there are new or unread messages or notifications." />
+      <div className=" mt-8 flex flex-row items-center justify-around w-full ">
+        <div className="badge">
+          <i class="fas fa-shopping-cart fa-4x"></i>
+          <div className="circle">
+            <span className="content">1</span>
+          </div>
+        </div>
+        <div className="badge">
+          <i class="fas fa-bell fa-4x"></i>
+          <div className="circle">
+            <span className="content">1</span>
+          </div>
+        </div>
+        <div className="badge">
+          <i class="fas fa-inbox fa-4x"></i>
+          <div className="circle">
+            <span className="content">1</span>
+          </div>
+        </div>
+      </div>
 
-<span class="badge"><i class="fa fa-bell"></i></span>
-
-<span class="badge"><i class="fa fa-inbox"></i></span>
-
-<span class="badge">Maddy</span>`}/>
-        </>
-    )
-}
+      <Snippet
+        code_snippet={`<div className="badge">
+    <i class="fas fa-inbox fa-4x"></i>
+    <div className="circle">
+        <span className="content">1</span>
+    </div>
+</div>`}
+      />
+    </>
+  );
+};
