ee2f2823454207ec9c1c05c5236a0473e35da711
Maddy@Xdev200 <xdev200200@gmail.com>
2021-06-02 12:04:37 +0530

    fix: class name to className

--- src/Components/Library/Badge/Badge.jsx
+++ src/Components/Library/Badge/Badge.jsx
@@ -8,19 +8,19 @@ export const Badge = () => {
       <Description text="Badges can notify you that there are new or unread messages or notifications." />
       <div className=" mt-8 flex flex-row items-center justify-around w-full ">
         <div className="wrapper">
-          <i class="fas fa-shopping-cart fa-4x"></i>
+          <i className="fas fa-shopping-cart fa-4x"></i>
           <div className="badge">
             <span className="content">1</span>
           </div>
         </div>
         <div className="wrapper">
-          <i class="fas fa-bell fa-4x"></i>
+          <i className="fas fa-bell fa-4x"></i>
           <div className="badge">
             <span className="content">1</span>
           </div>
         </div>
         <div className="wrapper">
-          <i class="fas fa-inbox fa-4x"></i>
+          <i className="fas fa-inbox fa-4x"></i>
           <div className="badge">
             <span className="content">1</span>
           </div>
