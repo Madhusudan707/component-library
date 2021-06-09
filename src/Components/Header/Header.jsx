bc7fa3e0fd900b420bfb912c1d175d5ea4074cd1
XDEV200 <xdev200200@gmail.com>
2021-06-09 11:17:15 +0530

    update: hide header in large devices

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './header.css'
 
 export const Header = () => {
     return (
-        <div className='flex items-center justify-center bg-blue-500 text-white p-4'>
+        <div className='flex items-center justify-center bg-blue-500 text-white p-4 lg:hidden'>
            <p className='text-5xl font-bold'>OK CSS</p>
         </div>
     )
